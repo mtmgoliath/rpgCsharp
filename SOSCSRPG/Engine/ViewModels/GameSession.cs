64457f883be36627ec6b713b5e495b038ef4af4f
Mike <mtmgoliath@gmail.com>
2021-06-24 00:35:25 +0100

    Created and linked engine models with WPFUI

---
+++ SOSCSRPG/Engine/ViewModels/GameSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModel
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Ivar Wolfsbane";
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
---
+++ SOSCSRPG/Engine1/ViewModels/GameSession.cs
@@ -0,0 +1,16 @@
+﻿using Engine.Models;
+
+namespace Engine.ViewModel
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Ivar Wolfsbane";
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
